a88c2418a8e829acf3029df940fd8cc9cb4d5ef5
bigazzzz <bigazzzz@gmail.com>
2019-10-25 16:32:59 +0300

    rename 8.function added 9.modules

---
+++ 8. Function/function_example_7.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def update_global_a():
+    global_a = 10 #+ global_a
+    return global_a
+
+global_a =5
+print(global_a)
+print(update_global_a())
+print(global_a)
---
+++ 9.Modules/example_circle_math_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from example_circle_math_module import circuit,circle_area
+
+r = int(input('радиус=>'))
+
+print('Длина окружности = '+ str(circuit(r)))
+print('Площадь окружности = '+ str(circle_area(r)))
+
